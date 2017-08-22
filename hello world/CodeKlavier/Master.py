91adcda799728b595b6eedfb62baf583ab280aa0
Rob van Loon <borrob@me.com>
2017-08-22 22:30:44 +0200

    Setup: transfer script to python package
    This is still work in progress. Trying to move the seperate scripts into a
    bigger and self-contained python package. This avoides having to re-implement
    the same things over and over again.

---
+++ hello world/CodeKlavier/Master.py
@@ -0,0 +1,87 @@
+import time
+import rtmidi
+
+class Setup(object):
+
+    def __init__(self):
+        self.__midiin = rtmidi.MidiIn()
+        self.__ports = self.__midiin.get_ports()
+
+    def print_welcome(self):
+        for i in range(1, 5):
+            num = 20+(1)
+            print('##'*num)
+            #TODO: add codeKlavier logo
+            time.sleep(0.2)
+        
+        print('\n', "Welcome to the Codeklavier...", '\n')
+    
+    def show_ports(self):
+        print("These are your detected MIDI devices:", '\n')
+        for port in self.__ports:
+            print(self.__ports.index(port), " -> ", port)
+    
+    def get_port_from_user(self):
+        selected_midiport = -1
+        while selected_midiport < 0:
+            try:
+                choice = input("Please choose the MIDI device (number) you want to use and hit Enter:")
+                selected_midiport = int(choice)
+                if selected_midiport < 0 or selected_midiport >= len(self.__ports):
+                    print("Invalid number, please try again:")
+                    selected_midiport = -1
+                else:
+                    return selected_midiport
+            except KeyboardInterrupt:
+                print('\n', "You want to quit? Fine with me!. Bye bye.")
+                exit()
+            except ValueError:
+                print("Sorry, type a valid port numer!")
+    
+    def open_port(self, pnum):
+        print("You have chosen: ", self.__ports[pnum])
+        
+        if self.__ports:
+            #TODO: do we need to check on the existence of ports?
+            self.__midiin.open_port(pnum)
+        else:
+            raise Exception("No midi ports! Don't know what to do!")
+
+    def close_port(self):
+        self.__midiin.close_port()
+
+    def get_message(self):
+        return self.__midiin.get_message()
+
+    def end(self):
+        print("Bye bye from CodeKlavier setup.")
+        self.close_port()
+        del self.__midiin
+
+def main():
+    codeK = Setup()
+
+    codeK.print_welcome()
+    codeK.show_ports()
+    my_midiport = codeK.get_port_from_user()
+    if my_midiport >= 0:
+        codeK.open_port(my_midiport)
+        print("CodeKlavier is ON. Press Control-C to exit.")
+        try:
+            timer = time.time()
+            while True:
+                msg = codeK.get_message()
+                
+                if msg:
+                    message, deltatime = msg
+                    print('deltatime: ', deltatime, 'msg: ', message)
+        
+                time.sleep(0.01)
+
+        except KeyboardInterrupt:
+            print('')
+        finally:
+            codeK.end()
+
+if __name__ == "__main__":
+    main()
