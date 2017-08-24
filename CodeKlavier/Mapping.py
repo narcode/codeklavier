a39a5ef298c5af0720b0b75c8fe6c85f82e5117a
Rob van Loon <borrob@me.com>
2017-08-24 22:06:48 +0200

    Add HelloWorld_NKK to module
    The NKK is slightly different from the normal HelloWorld. Perhaps it would be
    better to combine the two to a single file and have a small switch to select
    which one the player wants to use.
    
    The diffence in keyboard mapping is handled by the CodeKlavier/Mapping package.
    This allows easy reuse or changing of the mapping.

--- CodeKlavier/Mapping.py
+++ CodeKlavier/Mapping.py
@@ -91,3 +91,89 @@ class Mapping_HelloWorld:
             self.__keyboard.type('.play'),
         elif midinumber == 102:
             self.__keyboard.type('TempoClock.default')
+
+class Mapping_HelloWorld_NKK:
+
+    def __init__(self):
+        print("Using the Hello World mapping (NKK)")
+        self.__keyboard = Controller()
+
+    def evaluateSC(self, what):
+        if what == 'play':
+            self.__keyboard.type('.play')
+            with self.__keyboard.pressed(Key.shift):
+                self.__keyboard.press(Key.enter)
+                self.__keyboard.release(Key.enter)
+            self.__keyboard.press(Key.enter)
+            self.__keyboard.release(Key.enter)
+        elif what == 'stop':
+            self.__keyboard.type('.stop')
+            with self.__keyboard.pressed(Key.shift):
+                self.__keyboard.press(Key.enter)
+                self.__keyboard.release(Key.enter)
+            self.__keyboard.press(Key.enter)
+            self.__keyboard.release(Key.enter)
+        elif what == 'enter':
+                self.__keyboard.press(Key.enter)
+                self.__keyboard.release(Key.enter)
+    
+    def enter(self):
+        self.__keyboard.press(Key.enter)
+        self.__keyboard.release(Key.enter)
+    
+    def delete(self):
+        self.__keyboard.press(Key.backspace)
+        self.__keyboard.release(Key.backspace)
+    
+    
+    def mapping(midinumber):
+        # chars and nums
+        if midinumber == 87:
+            self.__keyboard.type('h')
+        elif midinumber == 92:
+            self.__keyboard.type('l')
+        elif midinumber == 90:
+            self.__keyboard.type('e')
+        elif midinumber ==94:
+            self.__keyboard.type('o')
+        elif midinumber == 95:
+            self.__keyboard.type('p')
+        elif midinumber == 91:
+            self.__keyboard.type('n')
+        elif midinumber == 89:
+            self.__keyboard.type('r')
+        elif midinumber == 84:
+            self.__keyboard.type('t')
+        elif midinumber == 83:
+            self.__keyboard.type('s')
+        elif midinumber == 80:
+            self.__keyboard.type('o')
+        elif midinumber == 102:
+            self.__keyboard.type('a')
+        elif midinumber == 104:
+            self.__keyboard.type('f')
+        elif midinumber == 106:
+            self.__keyboard.type('x')
+        elif midinumber == 88:
+            self.__keyboard.type('d')
+        elif midinumber == 89:
+            self.__keyboard.type('r')
+        elif midinumber == 103:
+            self.__keyboard.type('-'),
+        elif midinumber == 105:
+            self.__keyboard.type('+'),
+        elif midinumber == 107:
+            delete(),
+       # special keys
+        elif midinumber == 85:
+            self.__keyboard.type('~'),
+        elif midinumber == 96:
+            self.__keyboard.type('='),
+        elif midinumber == 98:
+            self.evaluateSC('stop')
+        elif midinumber == 99:
+            self.__keyboard.type('.tempo'),
+        elif midinumber == 97:
+            self.evaluateSC('play')
+        elif midinumber == 108:
+            self.evaluateSC('enter'),
