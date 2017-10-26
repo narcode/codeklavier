685deac5f53f37e162bf15a15f311e484e5a89e1
Narcode B <f.e.l.i.p.e@icloud.com>
2017-10-26 16:14:17 +0200

    start mottiptes dev

---
+++ motippets/motippets.py
@@ -0,0 +1,40 @@
+import time
+import rtmidi
+
+# Use trick to import from parentdir
+import sys
+import os
+import inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir)
+from CodeKlavier.Setup import Setup
+from CodeKlavier.Mapping import Mapping_HelloWorld
+from CodeKlavier.hello_classes import HelloWorld
+
+# Start the CodeKlavier
+codeK = Setup()
+myPort = codeK.perform_setup()
+codeK.open_port(myPort)
+device_id = codeK.get_device_id()
+print('your device id is: ', device_id, '\n')
+
+# Use your favourite mapping of the keys
+mapping = Mapping_HelloWorld()
+
+print("\nCodeKlavier is ready and ON.")
+print("You are performing: HELLO WORLD")
+print("\nPress Control-C to exit.")
+
+codeK.set_callback(HelloWorld(myPort, mapping, device_id))
+
+# Loop to program to keep listening for midi input
+try:
+    timer = time.time()
+    while True:
+        time.sleep(0.01)
+except KeyboardInterrupt:
+    print('')
+finally:
+    print("Bye-Bye :(")
+    codeK.end()
