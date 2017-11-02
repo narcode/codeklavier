161077011b7c84eaa340279fc833f158480c1450
Rob van Loon <borrob@me.com>
2017-11-02 21:28:48 +0100

    Move mottipets motifs
    The motifs are similar to 'Mappings'. Multiple versions/settings can co-exists
    next to each other no problem.

--- CodeKlavier/motippets_classes.py
+++ CodeKlavier/motippets_classes.py
@@ -5,7 +5,7 @@ Second prototype of the CodeKlavier
 """
 import rtmidi
 from functools import reduce
-from .motifs_interface import Motifs
+from CodeKlavier.Motifs import Motifs
 
 # class to handle the midi input
 class Motippets(object):
