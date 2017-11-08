1bc3b4c04ecacc5d9addfc95aa69733521883b01
Narcode B <f.e.l.i.p.e@icloud.com>
2017-11-08 01:50:59 +0100

    debugged and clean

--- motippets/motippets.py
+++ motippets/motippets.py
@@ -1,5 +1,4 @@
 import time
-import rtmidi
 from threading import Thread
 
 import sys
@@ -19,7 +18,8 @@ codeK.open_port(myPort)
 device_id = codeK.get_device_id()
 print('your device id is: ', device_id, '\n')
 
-# Use your favourite mapping of the keys
+# Use your favourite mapping of the keys0
+
 mapping = Mapping_Motippets()
 
 print("\nCodeKlavier is ready and ON.")
@@ -27,22 +27,22 @@ print("You are performing: Motippets")
 print("\nPress Control-C to exit.")
 
 # main memory (i.e. listen to the whole register)
-mainMem = Motippets(myPort, mapping, device_id)
+mainMem = Motippets(mapping, device_id)
 
 # midi listening per register
 pianosectons = [47, 78, 108]
-memLow = Motippets(myPort, mapping, device_id)
-memMid = Motippets(myPort, mapping, device_id)
-memHi = Motippets(myPort, mapping, device_id)
+memLow = Motippets(mapping, device_id)
+memMid = Motippets(mapping, device_id)
+memHi = Motippets(mapping, device_id)
 
 #midi listening for tremolos
-tremoloHi = Motippets(myPort, mapping, device_id)
-tremoloMid = Motippets(myPort, mapping, device_id)
-tremoloLow = Motippets(myPort, mapping, device_id)
+tremoloHi = Motippets(mapping, device_id)
+tremoloMid = Motippets(mapping, device_id)
+tremoloLow = Motippets(mapping, device_id)
 
 #midi listening for conditionals 
-conditionals = Motippets(myPort, mapping, device_id)
-noteBuffer = Motippets(myPort, mapping, device_id)
+conditionals = Motippets(mapping, device_id)
+noteBuffer = Motippets(mapping, device_id)
 
 #multiprocessing vars
 counter = 0
@@ -60,30 +60,31 @@ def parallelism(debug=True):
         
 # Loop to program to keep listening for midi input
 try:
-    timer = time.time()
     while True:
         msg = codeK.get_message()
 
         if msg:
             counter += 1
             
-            #motifs:
+            ##motifs:
             mainMem.parse_midi(msg, 'full')
             memLow.parse_midi(msg, 'low')
             memMid.parse_midi(msg, 'mid')
             memHi.parse_midi(msg, 'hi')
-            #tremolos:
+            ##tremolos:
             tremoloHi.parse_midi(msg, 'tremoloHi')
             tremoloMid.parse_midi(msg, 'tremoloMid')
             tremoloLow.parse_midi(msg, 'tremoloLow')
-            #conditionals               
             
+            ##conditionals              
             if conditionals.parse_midi(msg, 'conditionals') == "2 on":
                 counter = 0 # reset the counter
                 if __name__ == '__main__':
                     p = Thread(target=parallelism, name='conditional note counter thread')
                     p.start()      
-                    
+        
+        time.sleep(0.01)
+        
 except KeyboardInterrupt:
     print('')
 finally:
