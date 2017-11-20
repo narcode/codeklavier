6a18d3c33ece4405d2376908691157e7eb507561
Narcode B <f.e.l.i.p.e@icloud.com>
2017-11-20 11:04:23 +0100

    activesense fix for hello world

--- CodeKlavier/hello_classes.py
+++ CodeKlavier/hello_classes.py
@@ -14,8 +14,9 @@ class HelloWorld(object):
 
     def __call__(self, event, data=None):
         message, deltatime = event
-        if message[2] > 0: #only noteOn
-            if (message[0] == self.noteonid):
-                self.mapscheme.mapping(message[1])
-            if (message[0] == 176): #hardcoded pedal id (not pretty) TODO: make it dynamic
-                self.mapscheme.stopSC(message[1])
+        if message[0] != 254:
+            if message[2] > 0: #only noteOn
+                if (message[0] == self.noteonid):
+                    self.mapscheme.mapping(message[1])
+                if (message[0] == 176): #hardcoded pedal id (not pretty) TODO: make it dynamic
+                    self.mapscheme.stopSC(message[1])
