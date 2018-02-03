223d2fae828cfe94dbe6b94a626e8ce81a920298
Narcode B <f.e.l.i.p.e@icloud.com>
2018-02-03 16:11:48 +0100

    clean-up of deprecated files before release

--- CodeKlavier/Motifs.py
+++ CodeKlavier/Motifs.py
@@ -1,18 +1,6 @@
+# Motifs in Dictionaries. Way more efficient than an unjustified class (Thanks @borrob)
+
 motifs = {}
-motifs['motif_1'] = [30, 35, 36, 37, 38, 39, 40, 41, 42, 43, 42, 41, 40, 39, \
-                     38, 37, 36, 35]
-motifs['motif2'] = [63, 72, 60, 68, 51, 44, 60, 63, 61, 58, 55, 49]
-motifs['mini_motif_1_low'] = [24, 31, 32, 27, 26]
-motifs['mini_motif_2_low'] = [26, 32, 35, 38]
-motifs['mini_motif_1_mid'] = [60, 67, 68, 63, 62]
-motifs['mini_motif_2_mid'] = [50, 56, 59, 62]
-motifs['mini_motif_1_hi'] = [84, 91, 92, 87, 86]
-motifs['mini_motif_2_hi'] = [86, 92, 95, 98]
-motifs['conditional_1'] = [45, 43, 46, 45, 43, 41, 43, 45]
-motifs['conditional_2'] = [36, 31, 29, 26, 28, 35, 38, 33, 24, 26, 31, 28, 33, \
-                           21, 23, 29]
-motifs['conditional_result_1'] = [60, 62, 66, 70, 69, 67]
-motifs['conditional_result_2'] = [87, 80, 78, 68, 75, 79]
 
 # Motifs Anne
 motifs_anne = {}
