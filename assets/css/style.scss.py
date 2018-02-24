bb319d4fc9a28a8a200c47f5b14e7f77adecf3a8
Narcode B <f.e.l.i.p.e@icloud.com>
2018-02-24 15:10:10 +0100

    custom css for webpage

---
+++ assets/css/style.scss.py
@@ -0,0 +1,35 @@
+---
+---
+
+@import "{{ site.theme }}";
+.page-header {
+color: #fff;
+text-align: right;
+background-color: black;
+background-image: url('../images/codeklavier image.png');
+background-position: left;
+background-repeat: no-repeat;
+background-size: contain;
+}
+.project-name{
+text-shadow: 2px 2px black;
+}
+.project-tagline {
+opacity: 0.9;
+}
+.btn{
+color: rgb(255, 255, 255);
+background-color: rgba(25, 102, 128, 0.8);
+border-color: white;
+}
+.main-content h1 {
+  color: rgb(74, 118, 159);
+}
+a {
+color: #50c31d;
+}
+.dream {
+display: flex;
+flex-flow: column;
+padding:10px;
+}
