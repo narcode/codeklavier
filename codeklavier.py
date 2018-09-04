5114ce1b8c67deba80d51a5fffa04e52c8efc17e
Narcode B <f.e.l.i.p.e@icloud.com>
2018-09-04 13:32:14 +0200

    updated var name

--- codeklavier.py
+++ codeklavier.py
@@ -207,7 +207,7 @@ if __name__ == '__main__':
     #no arguments -> print help
     print('What do you want CodeKlavier to do? Give me something!')
     print('These prototypes are available:')
-    for p in PIECES:
+    for p in PROTOTYPES:
         print(' - ', p)
     print('')
     print('Here is how CodeKlavier works:')
