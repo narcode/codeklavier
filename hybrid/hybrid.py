085b2351c11f7bcd5ae0f31808bb0ced716d4719
Narcode B <f.e.l.i.p.e@icloud.com>
2019-01-07 17:59:25 +0100

    "pushing is overrated" - A. Veinberg

--- hybrid/hybrid.py
+++ hybrid/hybrid.py
@@ -603,6 +603,8 @@ def ck_loop(prototype='hello world'):
                             minimotif2_hi_mapped = memHi._unmapCounter2
 
                             ##tremolos:
+                            print('motif 2 played:', motif2_played)
+                            print('motif 1 played:', motif1_played)
                             print('mini3_mid:', minimotif3_mid_mapped)
                             print('mini2_mid:', minimotif2_mid_mapped)                            
                             if minimotif1_mid_mapped > 0:
