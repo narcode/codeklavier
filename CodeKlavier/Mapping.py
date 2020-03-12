e299e1ee8bae972f6c8026ef1d7e3d427830d9c2
Narcode B <f.e.l.i.p.e@icloud.com>
2020-03-12 22:32:12 +0100

    debug new shortcuts

--- CodeKlavier/Mapping.py
+++ CodeKlavier/Mapping.py
@@ -93,21 +93,23 @@ class Mapping_Motippets:
                 self.__keyboard.release(Key.enter)
         else:
             print(what)
-            if len(self._shortcuts[what] > 2):
+            if len(self._shortcuts[what] == 3):
                 with self.__keyboard.pressed(eval('Key.'+self._shortcuts[what][0].strip()),
                                              eval('Key.'+self._shortcuts[what][1].strip())):
                     if len(self._shortcuts[what][2].strip()) > 1:
                         self.__keyboard.press(eval('Key.'+self._shortcuts[what][2].strip()))
                     else:
-                        self.__keyboard.type(self._shortcuts[what][2].strip());
+                        self.__keyboard.type(self._shortcuts[what][2].strip())
                     self.__keyboard.release(eval('Key.'+self._shortcuts[what][2].strip()))
-            else:
+            elif len(self._shortcuts[what] == 2):
                 with self.__keyboard.pressed(eval('Key.'+self._shortcuts[what][0].strip())):
                     if len(self._shortcuts[what][2].strip()) > 1:
                         self.__keyboard.press(eval('Key.'+self._shortcuts[what][1].strip()))
                     else:
                         self.__keyboard.type(self._shortcuts[what][1].strip());
-                    self.__keyboard.release(eval('Key.'+self._shortcuts[what][1].strip()))                
+                    self.__keyboard.release(eval('Key.'+self._shortcuts[what][1].strip()))
+            else:
+                self.__keyboard.pressed(eval('Key.'+self._shortcuts[what][0].strip()))
 
     def goDown(self, display=5):
         """Press command-arrow down and enter.
@@ -234,6 +236,7 @@ class Mapping_Motippets:
         displays = [1,2]
         try:
             evaluate = self._config['shortcuts mapping'].get(motif)
+            print(evaluate)
         except KeyError:
             print('default eval')
             evaluate = 'eval'
