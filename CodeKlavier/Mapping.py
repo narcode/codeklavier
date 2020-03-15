9f3c5123a97ad3835da89bee1de546ebf910e27f
Narcode B <f.e.l.i.p.e@icloud.com>
2020-03-15 15:19:23 +0100

    anne anne anne

--- CodeKlavier/Mapping.py
+++ CodeKlavier/Mapping.py
@@ -245,6 +245,7 @@ class Mapping_Motippets:
         try:
             display = self._config['motippets display settings'].getint(motif)                         
             snippet = self._config['snippets code output'].get(motif)
+            print('debug:', snippet)
             if evaluate == 'eval':
                 self.__keyboard.type(snippet)
                 self.formatAndSend(snippet, display=display, syntax_color='snippet:')
