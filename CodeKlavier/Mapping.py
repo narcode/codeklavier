b1eadb70a54cac0f27b4d9de2ee71419b749dc29
Narcode B <f.e.l.i.p.e@icloud.com>
2020-03-25 14:42:50 +0100

    debug message sout

--- CodeKlavier/Mapping.py
+++ CodeKlavier/Mapping.py
@@ -82,7 +82,6 @@ class Mapping_Motippets:
                 self.__keyboard.release(Key.cmd)
         elif what == 'eval':
             if len(self._shortcuts[what]) == 1:
-                print(self._shortcuts[what][0].strip())
                 if len(self._shortcuts[what][0].strip()) > 1:
                     self.__keyboard.press(eval('Key.'+self._shortcuts[what][0].strip()))
                 else:
