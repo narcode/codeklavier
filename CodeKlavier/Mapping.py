ce1e094ed3245fe0ba9058b7911ce578011f39d4
Narcode B <f.e.l.i.p.e@icloud.com>
2020-04-08 10:11:16 +0200

    better function for getting the eval shortcuts

--- CodeKlavier/Mapping.py
+++ CodeKlavier/Mapping.py
@@ -83,7 +83,10 @@ class Mapping_Motippets:
         elif what == 'eval':
             if len(self._shortcuts[what]) == 1:
                 if len(self._shortcuts[what][0].strip()) > 1:
-                    self.__keyboard.press(eval('Key.'+self._shortcuts[what][0].strip()))
+                    if self._shortcuts[what][0].strip() == 'none': #TODO: make a better function
+                        self.parseEvaluate('eval_manual')
+                    else:
+                        self.__keyboard.press(eval('Key.'+self._shortcuts[what][0].strip()))
                 else:
                     self.__keyboard.type(self._shortcuts[what][0].strip())
             else:        
@@ -100,24 +103,30 @@ class Mapping_Motippets:
                 self.__keyboard.press(Key.enter)
                 self.__keyboard.release(Key.enter)
         else:
-            if len(self._shortcuts[what]) == 3:
-                with self.__keyboard.pressed(eval('Key.'+self._shortcuts[what][0].strip()),
-                                             eval('Key.'+self._shortcuts[what][1].strip())):
-                    if len(self._shortcuts[what][2].strip()) > 1:
-                        self.__keyboard.press(eval('Key.'+self._shortcuts[what][2].strip()))
-                    else:
-                        self.__keyboard.type(self._shortcuts[what][2].strip())
+            self.parseEvaluate(what)
+
+
+    def parseEvaluate(self, what):
+        """ parse the evaluate code from the ini file"""
+        if len(self._shortcuts[what]) == 3:
+            with self.__keyboard.pressed(eval('Key.'+self._shortcuts[what][0].strip()),
+                                         eval('Key.'+self._shortcuts[what][1].strip())):
+                if len(self._shortcuts[what][2].strip()) > 1:
+                    self.__keyboard.press(eval('Key.'+self._shortcuts[what][2].strip()))
+                else:
+                    self.__keyboard.type(self._shortcuts[what][2].strip())
+                
+        elif len(self._shortcuts[what]) == 2:
+            with self.__keyboard.pressed(eval('Key.'+self._shortcuts[what][0].strip())):
+                if len(self._shortcuts[what][1].strip()) > 1:
+                    self.__keyboard.press(eval('Key.'+self._shortcuts[what][1].strip()))
+                else:
+                    self.__keyboard.type(self._shortcuts[what][1].strip());
                     
-            elif len(self._shortcuts[what]) == 2:
-                with self.__keyboard.pressed(eval('Key.'+self._shortcuts[what][0].strip())):
-                    if len(self._shortcuts[what][1].strip()) > 1:
-                        self.__keyboard.press(eval('Key.'+self._shortcuts[what][1].strip()))
-                    else:
-                        self.__keyboard.type(self._shortcuts[what][1].strip());
-                        
-            else:
-                self.__keyboard.press(eval('Key.'+self._shortcuts[what][0].strip()))
-                self.__keyboard.release(eval('Key.'+self._shortcuts[what][0].strip()))
+        else:
+            self.__keyboard.press(eval('Key.'+self._shortcuts[what][0].strip()))
+            self.__keyboard.release(eval('Key.'+self._shortcuts[what][0].strip()))        
+        
 
     def goDown(self, display=5):
         """Press command-arrow down and enter.
