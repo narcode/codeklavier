987a2adfc0b9f948e755ca8afafe63f005124adb
Narcode B <f.e.l.i.p.e@icloud.com>
2020-04-08 13:27:33 +0200

    debug msg out

--- CodeKlavier/Mapping.py
+++ CodeKlavier/Mapping.py
@@ -124,7 +124,6 @@ class Mapping_Motippets:
                     self.__keyboard.type(self._shortcuts[what][1].strip());
                     
         else:
-            print(len(self._shortcuts[what][0].strip()))
             if len(self._shortcuts[what][0].strip()) > 1:
                 self.__keyboard.press(eval('Key.'+self._shortcuts[what][0].strip()))
                 self.__keyboard.release(eval('Key.'+self._shortcuts[what][0].strip()))      
